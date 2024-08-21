df70cfaa02e9b76dbab3a1d8502a23c056968acd
jakubkanna <hello.jakubkanna@gmail.com>
2024-08-21 08:33:59 +0200

    links added to homepage

--- src/pages/single/Home.tsx
+++ src/pages/single/Home.tsx
@@ -1,27 +1,50 @@
 import { useContext } from "react";
 import Layout from "../../components/layout/Layout.";
 import { GeneralContext } from "../../contexts/GeneralContext";
-import { Container, Row } from "react-bootstrap";
+import { Container, ListGroup, Row } from "react-bootstrap";
 import Image from "../../components/Image";
-import { ImageRefSchema } from "@jakubkanna/labguy-front-schema";
+import { ImageRefSchema, UrlSchema } from "@jakubkanna/labguy-front-schema";
+import { Link } from "react-router-dom";
 
 export default function Homepage() {
   const { preferences } = useContext(GeneralContext);
-  if (!preferences) return;
-  const { homepage_heading, homepage_subheading, homepage_background_image } =
-    preferences;
+  if (!preferences) return null;
+  const {
+    homepage_heading,
+    homepage_subheading,
+    homepage_background_image,
+    homepage_urls,
+  } = preferences;
 
   return (
     <Layout title="">
       <Container>
         <Row>
-          <Image image={homepage_background_image as ImageRefSchema}></Image>
+          {homepage_background_image && (
+            <Image
+              image={homepage_background_image[0] as ImageRefSchema}
+            ></Image>
+          )}
         </Row>
         <Row>
           <h1>{homepage_heading}</h1>
         </Row>
         <Row>
           <h2>{homepage_subheading}</h2>
+        </Row>
+        <Row>
+          <ListGroup horizontal>
+            {homepage_urls &&
+              homepage_urls.map((url) => {
+                return (
+                  <ListGroup.Item>
+                    <Link to={(url as UrlSchema).url} target="_blank">
+                      {(url as UrlSchema).title}
+                    </Link>
+                  </ListGroup.Item>
+                );
+              })}
+          </ListGroup>
         </Row>
       </Container>
     </Layout>
