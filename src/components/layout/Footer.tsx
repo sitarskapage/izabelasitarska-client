a0c127922cd3a3e1f8f821edc2367f5546fb733d
jakubkanna <hello.jakubkanna@gmail.com>
2024-11-15 16:35:57 +0100

    navbar tooltips

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,14 @@
 import { useContext, useState } from "react";
-import { Navbar, Collapse, Button, Row, Col } from "react-bootstrap";
+import {
+  Navbar,
+  Collapse,
+  Button,
+  Row,
+  Col,
+  OverlayTrigger,
+  Tooltip,
+  TooltipProps,
+} from "react-bootstrap";
 import { GeneralContext } from "../../contexts/GeneralContext";
 import { Link } from "react-router-dom";
 
@@ -17,21 +26,44 @@ export default function Footer() {
     { label: "Contact", path: "contact" },
   ];
 
+  const renderTooltip = (props: TooltipProps) => (
+    <Tooltip id="button-tooltip" {...props}>
+      Homepage
+    </Tooltip>
+  );
+  const renderTooltip2 = (props: TooltipProps) => (
+    <Tooltip id="button-tooltip" {...props}>
+      Menu
+    </Tooltip>
+  );
+
   return (
     <footer className="container-fluid position-fixed bottom-0 start-0 bg-kanna w-100 border-top border-dark">
       <nav className="d-flex justify-content-between align-items-center">
-        <Navbar.Brand className="text-uppercase">
-          <Link to={"/"}>{artists_name}</Link>
-        </Navbar.Brand>
-        <Button
-          onClick={() => setOpen(!open)}
-          aria-controls="CollapseMenu"
-          aria-expanded={open}
-          className="p-0 flex-grow-1 d-flex justify-content-end"
-          variant="link"
+        <OverlayTrigger
+          overlay={renderTooltip}
+          placement="right"
+          delay={{ show: 1000, hide: 400 }}
         >
-          <i className="bi bi-list fs-2 text-dark"></i>
-        </Button>
+          <Navbar.Brand className="text-uppercase">
+            <Link to={"/"}>{artists_name}</Link>
+          </Navbar.Brand>
+        </OverlayTrigger>
+        <OverlayTrigger
+          overlay={renderTooltip2}
+          placement="top"
+          delay={{ show: 1000, hide: 10 }}
+        >
+          <Button
+            onClick={() => setOpen(!open)}
+            aria-controls="CollapseMenu"
+            aria-expanded={open}
+            className="p-0 flex-grow-1 d-flex justify-content-end"
+            variant="link"
+          >
+            <i className="bi bi-list fs-2 text-dark"></i>
+          </Button>
+        </OverlayTrigger>
       </nav>
 
       <Collapse in={open} className="">
