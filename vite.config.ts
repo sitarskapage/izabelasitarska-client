a2c4b74a53ac7158eb5297db38fff09e37c25345
jakubkanna <hello.jakubkanna@gmail.com>
2025-01-05 20:56:31 +0100

    change base url

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/jakubkanna-client",
+  base: "jakubkanna.com",
 
   plugins: [
     react(),
