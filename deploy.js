bcf8b92e240ce0aff3cd62145ac554a622646395
jakubkanna <hello.jakubkanna@gmail.com>
2025-01-16 14:23:01 +0100

    remove gh-pages

--- deploy.js
+++
@@ -1,9 +0,0 @@
-import { publish } from "gh-pages";
-
-/**
- * The usage below will add a `CNAME` file to the output.
- */
-publish("dist", { cname: "example.com" }, (err) => {
-  if (err) throw new Error(err);
-  console.log("Published");
-});
